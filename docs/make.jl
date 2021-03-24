975ce8eeee5b00c20daf262859e2275e7b6ddadd
torfjelde <tor.erlend95@gmail.com>
2021-03-24 18:07:48 +0100

    remove publishing of docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,18 +6,14 @@ DocMeta.setdocmeta!(Epimap, :DocTestSetup, :(using Epimap); recursive=true)
 makedocs(;
     modules=[Epimap],
     authors="Tor Erlend Fjelde, Micheal J. Hutchinson, Yee Whye Teh, Hong Ge",
-    repo="https://github.com/torfjelde/Epimap.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/epimap/Epimap.jl/blob/{commit}{path}#{line}",
     sitename="Epimap.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://torfjelde.github.io/Epimap.jl",
+        canonical="https://epimap.github.io/Epimap.jl",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
     ],
-)
-
-deploydocs(;
-    repo="github.com/torfjelde/Epimap.jl",
 )
