2837b6dfc0bd322c19c27ef9050d9fee77fa256e
torfjelde <tor.erlend95@gmail.com>
2021-04-02 15:53:10 +0200

    fixed nnlib conv, i.e. removed buggy support for 2d version

--- test/conv.jl
+++ test/conv.jl
@@ -5,12 +5,7 @@
     @test Epimap.conv_fft(x, w) ≈ Epimap.conv_nnlib(x, w)
 
     # Matrix × Vector
-    x = randn(10, 2); w = randn(1, 2);
-    @test Epimap.conv_dsp(x, w) ≈ Epimap.conv_fft(x, w)
-    @test Epimap.conv_fft(x, w) ≈ Epimap.conv_nnlib(x, w)
-
-    # Matrix × Matrix
-    x = randn(10, 2); w = randn(2, 2);
+    x = randn(10, 2); w = randn(2);
     @test Epimap.conv_dsp(x, w) ≈ Epimap.conv_fft(x, w)
     @test Epimap.conv_fft(x, w) ≈ Epimap.conv_nnlib(x, w)
 end
