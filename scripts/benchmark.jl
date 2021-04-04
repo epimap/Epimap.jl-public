5ec1ed8f9d78fa8433676a9c1ecc796a957206c6
torfjelde <tor.erlend95@gmail.com>
2021-04-04 21:48:29 +0200

    updated benchmark script to reflect changes

--- scripts/benchmark.jl
+++ scripts/benchmark.jl
@@ -29,10 +29,10 @@ default_args = (
 setup_args = merge(Rmap.setup_args(
     Rmap.rmap_naive, 
     data, 
-    first_day_modelled = Date("2020-10-01", "y-m-d"), 
-    steps_modelled = 15, 
-    days_per_step = 7,
-    conditioning_days = 30
+    first_date = Date("2020-10-01", "y-m-d"),
+    num_steps = 15,
+    timestep = Week(1),
+    num_condition_days = 30
 ), default_args);
 
 # Instantiate model
