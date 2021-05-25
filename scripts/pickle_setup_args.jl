6f30621e1b03caa9275e43019bbe51fd64a0fdad
torfjelde <tor.erlend95@gmail.com>
2021-05-25 01:45:58 +0100

    improved the pickle-args script

--- scripts/pickle_setup_args.jl
+++ scripts/pickle_setup_args.jl
@@ -1,14 +1,46 @@
-using Epimap, Dates
+using ArgParse, Dates, Serialization
+using Epimap
 using PyCall
 
-const out_path = "setup_args.pkl"
+# TODO: Add a bunch of arguments to allow further specification of the data,
+# e.g. number of steps, the size of each timestep.
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "out-path"
+    help = "path where the generated model args should go"
+    "--python"
+    action = :store_true
+    help = "if specified, will output model arg to pickled file with suffix '.pkl'"
+    "--julia"
+    action = :store_true
+    help = "if specified, will output model arg to serialized file with suffix '.jls'"
+end
+
+parsed_args = parse_args(s)
+out_path = parsed_args["out-path"]
+for_python = parsed_args["python"]
+for_julia = parsed_args["julia"]
+
+# Determinte the output paths.
+directory_path = dirname(out_path)
+filename = if isdirpath(out_path)
+    println("Given output path is a directory; using default basename 'model_args'")
+    "model_args"
+else
+    # If we're storing both, then we remove the extension.
+    (for_python && for_julia) ? splitext(basename(out_path))[1] : basename(out_path)
+end
+
+out_path_python = joinpath(directory_path, filename * ".pkl")
+out_path_julia = joinpath(directory_path, filename * ".jls")
 
 # Load data and construct `setup_args`.
-print("Loading data...")
+println("Loading data...")
 data = Rmap.load_data()
 println("OK!")
 
-print("Constructing model args...")
+println("Constructing model args...")
 setup_args = Rmap.setup_args(
     Rmap.rmap_naive, data, Float32,
     num_steps = 15,
@@ -16,27 +48,34 @@ setup_args = Rmap.setup_args(
 )
 println("OK!")
 
-# Convert the values in `setup_args` to corresponding python objects.
-print("Converting to Python objects...")
-setup_args_py = map(setup_args) do v
-    if v isa AbstractArray
-        # Reverse dims so get row-major ordering as is standard in Python.
-        PyObject(PyReverseDims(Array(v)))
-    else
-        PyObject(v)
+if for_python
+    # Convert the values in `setup_args` to corresponding python objects.
+    print("Converting to Python objects...")
+    setup_args_py = map(setup_args) do v
+        if v isa AbstractArray
+            # Reverse dims so get row-major ordering as is standard in Python.
+            PyObject(PyReverseDims(Array(v)))
+        else
+            PyObject(v)
+        end
+    end;
+
+    # Convert to a `Dict`, as it is more comon in Pyton.
+    setup_args_py_dict = PyObject(Dict(pairs(setup_args_py)...));
+    println("OK!")
+
+    # Import `pickle`.
+    pkl = pyimport("pickle")
+
+    # Pickle the full `setup_args_py_dict`.
+    print("Pickling model args to $(out_path_python)...")
+    @pywith pybuiltin("open")(out_path_python, "wb+") as f begin
+        pkl.dump(setup_args_py_dict, f)
     end
-end;
-
-# Convert to a `Dict`, as it is more comon in Pyton.
-setup_args_py_dict = PyObject(Dict(pairs(setup_args_py)...));
-println("OK!")
-
-# Import `pickle`.
-pkl = pyimport("pickle")
-
-# Pickle the full `setup_args_py_dict`.
-print("Pickling model args to $(out_path)...")
-@pywith pybuiltin("open")(out_path, "wb+") as f begin
-    pkl.dump(setup_args_py_dict, f)
+    println("OK!")
 end
-println("Done!")
+
+if for_julia
+    print("Serializing model args to $(out_path_julia)...")
+    serialize(out_path_julia, setup_args)
+end
