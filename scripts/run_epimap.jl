47da69305e1dfdeb1c50c7f0732c907a5ec3da68
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2021-07-02 01:26:14 +0100

    updated run_epimap.jl and added TensorBoard logging

--- scripts/run_epimap.jl
+++ scripts/run_epimap.jl
@@ -3,6 +3,7 @@ using AdvancedHMC, Zygote
 using DataFrames
 using Dates
 using Adapt
+using TuringCallbacks
 
 using Serialization, DrWatson, Dates
 
@@ -26,23 +27,27 @@ intermediatedir(args...) = joinpath(intermediatedir(), args...)
 mkpath(intermediatedir())
 
 # Load data
-const DATADIR = "file://" * joinpath(ENV["HOME"], "Projects", "private", "epimap-data", "processed_data")
+# const DATADIR = "file://" * joinpath(ENV["HOME"], "Projects", "private", "epimap-data", "processed_data")
+const DATADIR = "file://" * joinpath(ENV["HOME"], "Projects", "private", "Rmap", "data")
 data = Rmap.load_data(get(ENV, "EPIMAP_DATA", DATADIR));
 T = Float32
 
 # Construct the model arguments from data
-setup_args = Rmap.setup_args(
+setup_args, dates = Rmap.setup_args(
     Rmap.rmap_naive, data, T;
     num_steps = 15,
-    timestep = Week(1)
+    timestep = Week(1),
+    include_dates = true
 )
+
+serialize(intermediatedir("dates.jls"), dates)
 
 # Arguments not related to the data which are to be set up
 default_args = (
     ρ_spatial = 10.0,
     ρ_time = 0.1,
-    σ_spatial = 0.1,
-    σ_local = 0.1,
+    σ_spatial = missing,
+    σ_local = missing,
     σ_ξ = 1.0
 )
 
@@ -51,7 +56,7 @@ serialize(intermediatedir("args.jls"), args)
 
 # Instantiate model
 m = Rmap.rmap_naive(args...);
-logπ, logπ_unconstrained, b, θ_init = Epimap.make_logjoint(Rmap.rmap_naive, args..., Matrix{T});
+logπ, logπ_unconstrained, b, θ_init = Epimap.make_logjoint(m);
 const b⁻¹ = inv(b)
 
 # Give it a try
@@ -88,7 +93,7 @@ adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(T(0.8), inte
 
 # Set-up for AbstractMCMC.
 import AdvancedHMC: AbstractMCMC
-rng = Turing.Random.MersenneTwister(42)
+rng = Turing.Random.MersenneTwister(43)
 
 sampler = AdvancedHMC.HMCSampler(κ, metric, adaptor)
 model = AdvancedHMC.DifferentiableDensityModel(hamiltonian.ℓπ, hamiltonian.∂ℓπ∂θ);
@@ -98,7 +103,30 @@ nadapts = 1_000;
 nsamples = 1_000;
 
 # Callback to use for progress-tracking.
-cb = AdvancedHMC.HMCProgressCallback(nadapts + nsamples, progress = true, verbose = false)
+cb1 = AdvancedHMC.HMCProgressCallback(nadapts + nsamples, progress = true, verbose = false)
+
+using TuringCallbacks.OnlineStats
+stat = Series(
+    # Estimators using the entire chain
+    Series(Mean(), Variance(), AutoCov(10), KHist(100)),
+    # Estimators using only the last 1000 samples
+    WindowStat(100, Series(Mean(), Variance(), AutoCov(10), KHist(100)))
+)
+cb2 = TensorBoardCallback("tensorboard_logs/run", stat, include = ["ψ", "ξ", "σ_spatial", "σ_local"])
+
+# HACK: Super-hacky impl. Should improve `TuringCallbacks` to be more flexible instead.
+function Turing.Inference._params_to_array(ts::Vector{<:SimpleTransition})
+    @assert length(ts) == 1
+    # Convert to `Float64` because otherwise TuringCallbacks won't be happy.
+    nt = NamedTuple(Float64.(ts[1].θ))
+    return keys(nt), map(values(nt)) do x
+        return if length(x) == 1
+            first(x)
+        else
+            x
+        end
+    end
+end
 
 # Create the iterator.
 it = AbstractMCMC.steps(
@@ -113,7 +141,10 @@ transition, state = iterate(it);
 @info "Initial sample" transition.stat
 
 # Create the sample container.
-samples = AbstractMCMC.samples(SimpleTransition(b⁻¹(transition.z.θ), transition.stat), model, sampler);
+samples = let
+    θ, logjac = forward(b⁻¹, transition.z.θ)
+    AbstractMCMC.samples(SimpleTransition(θ, transition.z.ℓπ.value + logjac, transition.stat), model, sampler);
+end
 
 # [OPTIONAL] Keep track of some states for debugging purposes.
 states = [state];
@@ -126,7 +157,7 @@ states = [state];
     transition, state = iterate(it, state)
     
     # Run callback
-    cb(rng, model, sampler, transition, state, state.i; it.kwargs...)
+    cb1(rng, model, sampler, transition, state, state.i; it.kwargs...)
 
     # Save some of the states just for fun
     if i % 200 == 0
@@ -135,7 +166,11 @@ states = [state];
     
     # Save sample
     # Transform `transition` back to original space.
-    t = SimpleTransition(b⁻¹(transition.z.θ), transition.z.ℓπ.value, transition.stat)
+    t = let
+        θ, logjac = forward(b⁻¹, transition.z.θ)
+        SimpleTransition(θ, transition.z.ℓπ.value + logjac, transition.stat)
+    end
+    cb2(rng, model, sampler, t, state, state.i; it.kwargs...)
     AbstractMCMC.save!!(samples, t, state.i, model, sampler, nsamples)
 end
 
@@ -153,7 +188,7 @@ serialize(intermediatedir("kwargs_adaptation.jls"), it.kwargs)
     transition, state = iterate(it, state)
     
     # Run callback
-    cb(rng, model, sampler, transition, state, state.i; it.kwargs...)
+    cb1(rng, model, sampler, transition, state, state.i; it.kwargs...)
 
     # Save some of the states just for fun
     if i % 200 == 0
@@ -162,6 +197,7 @@ serialize(intermediatedir("kwargs_adaptation.jls"), it.kwargs)
     
     # Save sample
     t = SimpleTransition(b⁻¹(transition.z.θ), transition.z.ℓπ.value, transition.stat)
+    cb2(rng, model, sampler, t, state, state.i; it.kwargs...)
     AbstractMCMC.save!!(samples, t, state.i, model, sampler, nsamples)
 end
 
