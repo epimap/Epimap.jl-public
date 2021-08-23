6011ae48f6228bde7cfdf68ed44de206db7ab7b7
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-08-24 00:02:15 +0100

    reverted change to logjoint for rmap_naive

--- src/models/rmap/models.jl
+++ src/models/rmap/models.jl
@@ -866,8 +866,7 @@ function DynamicPPL.logjoint(
     precomputed,
     args
 )
-    new_args = merge(DynamicPPL.getvalue_nested(model.context), NamedTuple(args))
-    return DynamicPPL.logjoint(model, precomputed, new_args, Val{keys(new_args)}())
+    return DynamicPPL.logjoint(model, precomputed, args, Val{keys(args)}())
 end
 
 @generated function DynamicPPL.logjoint(
